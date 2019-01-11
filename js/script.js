c09e6c8d94546fc05319eaf6b031362911273255
zslvsk <44968375+zslvsk@users.noreply.github.com>
2019-01-11 20:49:44 +0300

    подготовила проект ко второй проверке

--- js/script.js
+++ js/script.js
@@ -14,52 +14,53 @@
         var storage2 = "";
   
         try {
-        storage2 = localStorage.getItem("email");
-        storage = localStorage.getItem("identity");
+          storage2 = localStorage.getItem("email");
+          storage = localStorage.getItem("identity");
         } catch (err) {
-        isStorageSupport = false;
+          isStorageSupport = false;
         }
 
         button.addEventListener("click", function (evt) {
           evt.preventDefault();
           popup.classList.add("feedback-show");
           overlay.classList.add("overlay-show");
+
           if (storage) {
             identity.value = storage;
             email.value = storage2;
-        }
-          claim.focus();
+          }
+            claim.focus();
         });
 
         close.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        popup.classList.remove("feedback-show");
-        overlay.classList.remove("overlay-show");
-        popup.classList.remove("feedback-error");
+          evt.preventDefault();
+          popup.classList.remove("feedback-show");
+          overlay.classList.remove("overlay-show");
+          popup.classList.remove("feedback-error");
         });
 
         form.addEventListener("submit", function (evt) {
-        if (!identity.value || !email.value || !claim.value) {
-        evt.preventDefault();
-        popup.classList.remove("feedback-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("feedback-error");
-        console.log("Нужно заполнить все поля формы");
-        } else {
-           if (isStorageSupport) {
-            localStorage.setItem("identity", identity.value);
-            localStorage.setItem("email", email.value);
-        }
-        }   
+          if (!identity.value || !email.value || !claim.value) {
+            evt.preventDefault();
+            popup.classList.remove("feedback-error");
+            popup.offsetWidth = popup.offsetWidth;
+            popup.classList.add("feedback-error");
+            console.log("Нужно заполнить все поля формы");
+          } else {
+            if (isStorageSupport) {
+              localStorage.setItem("identity", identity.value);
+              localStorage.setItem("email", email.value);
+            }
+          }   
         });
 
         window.addEventListener("keydown", function (evt) {
-        if (evt.keyCode === 27) {
-        evt.preventDefault();
-        if (popup.classList.contains("feedback-show")) {
-        popup.classList.remove("feedback-show");
-        overlay.classList.remove("overlay-show");
-        popup.classList.remove("feedback-error");
-        }
-        }
+          if (evt.keyCode === 27) {
+            evt.preventDefault();
+            if (popup.classList.contains("feedback-show")) {
+              popup.classList.remove("feedback-show");
+              overlay.classList.remove("overlay-show");
+              popup.classList.remove("feedback-error");
+            }
+          }
         });
